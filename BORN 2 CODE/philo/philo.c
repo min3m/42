6ebb056eed27e6bf93d94caa6e0aa9d259cb67e0
min3m <min3m955@naver.com>
2023-02-04 16:06:14 +0900

    .

--- BORN 2 CODE/philo/philo.c
+++ BORN 2 CODE/philo/philo.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   philo.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: youngmin <youngmin@student.42.fr>          +#+  +:+       +#+        */
+/*   By: youngmch <youngmch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/16 22:09:24 by youngmch          #+#    #+#             */
-/*   Updated: 2023/01/21 18:02:10 by youngmin         ###   ########.fr       */
+/*   Updated: 2023/01/26 18:33:08 by youngmch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,6 @@
 int	main(int argc, char **argv)
 {
 	if (argc != 5 && argc != 6)
-		return(ft_putendl_fd("Require 5 or 6 argument!"));
+		return (ft_putendl_fd("Require 5 or 6 argument!"));
 	return (0);
 }
--- BORN 2 CODE/philo/philo.h
+++ BORN 2 CODE/philo/philo.h
@@ -3,25 +3,34 @@
 /*                                                        :::      ::::::::   */
 /*   philo.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: youngmin <youngmin@student.42.fr>          +#+  +:+       +#+        */
+/*   By: youngmch <youngmch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/16 22:10:14 by youngmch          #+#    #+#             */
-/*   Updated: 2023/01/19 22:01:31 by youngmin         ###   ########.fr       */
+/*   Updated: 2023/01/30 17:22:41 by youngmch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILO_H
 # define PHILO_H
 
-#include <stdio.h>
-#include <unistd.h>
-#include <pthread.h>
-#include <stdlib.h>
-#include <sys/time.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <pthread.h>
+# include <stdlib.h>
+# include <sys/time.h>
 
 /* libft.c */
 
 static int	ft_isspace(const char *nptr, int i);
 static int	ft_atoi(const char *nptr);
+
+typedef struct s_philo
+{
+	int	philo_num;
+	int	time_to_die;
+	int	time_to_eat;
+	int	time_to_sleep;
+	int	min_count;
+}				t_philo;
 
 #endif
