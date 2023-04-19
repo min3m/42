526f16dbe5c8364e98d7249043218df7f3af95a2
min3m <min3m955@naver.com>
2023-04-19 17:01:14 +0900

    ex00 fin

--- BORN 2 CODE/cpp_module/cpp00/ex00/megaphone.cpp
+++ BORN 2 CODE/cpp_module/cpp00/ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: youngmch <youngmch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/27 18:45:20 by youngmch          #+#    #+#             */
-/*   Updated: 2023/04/17 19:29:36 by youngmch         ###   ########.fr       */
+/*   Updated: 2023/04/19 17:00:31 by youngmch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,15 @@
 int	main(int argc, char *argv[])
 {
 	if (argc == 1)
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
 	else
 	{
-		for (int i = 0; i < argc; i++)
-			for (int j = 0; j < strlen(argv[i]); j++)
-				std::cout << toupper(argv[i][j]) << std::endl;
+		for (int i = 1; i < argc; i++)
+		{
+			for (size_t j = 0; j < strlen(argv[i]); j++)
+				std::cout << static_cast<char>(toupper(argv[i][j]));
+		}
 	}
+	std::cout << std::endl;
 	return (0);
 }
