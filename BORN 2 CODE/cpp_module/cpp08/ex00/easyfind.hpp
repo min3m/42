c771d0150ab2be4d0cdcf50bb33a1532b4369899
min3m <min3m955@naver.com>
2023-05-24 21:43:55 +0900

    cpp08/ex00 fin remember const T& can't compile

--- BORN 2 CODE/cpp_module/cpp08/ex00/easyfind.hpp
+++ BORN 2 CODE/cpp_module/cpp08/ex00/easyfind.hpp
@@ -1,13 +1,31 @@
 #ifndef EASYFIND_HPP
 #define EASYFIND_HPP
 
+#include <algorithm>
+#include <deque>
+#include <iostream>
+#include <list>
 #include <exception>
+#include <vector>
+
+class NotFoundException : public std::exception
+{
+public:
+	virtual const char *what() const throw()
+	{
+		return ("Easyfind can't find value in this container");
+	}
+};
+
 
 template <typename T>
-typename T::std::iterator easyfind()
+typename T::iterator easyfind(T &source, int value)
 {
+	typename T::iterator iter = std::find(source.begin(), source.end(), value);
 
-	return ()
+	if (iter == source.end())
+		throw NotFoundException();
+	return (iter);
 }
 
 #endif
--- BORN 2 CODE/cpp_module/cpp08/ex00/main.cpp
+++ BORN 2 CODE/cpp_module/cpp08/ex00/main.cpp
@@ -2,5 +2,45 @@
 
 int main()
 {
+	std::vector<int> v;
+	std::list<int> l;
+	std::deque<int> d;
+
+	for (int i = 0; i < 10; i++)
+	{
+		v.push_back(i);
+		l.push_back(i);
+		d.push_back(i);
+	}
+	try
+	{
+		std::vector<int>::iterator find;
+		find = easyfind(v, 5);
+		std::cout << *find << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	try
+	{
+		std::list<int>::iterator find;
+		find = easyfind(l, 9);
+		std::cout << *find << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	try
+	{
+		std::deque<int>::iterator find;
+		find = easyfind(d, 11);
+		std::cout << *find << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
 	return (0);
 }
